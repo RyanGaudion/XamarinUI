7777dc6d22b8f09ba8d31d61c77e403928db3bf2
Ryan Gaudion <43444172+RyanGaudion@users.noreply.github.com>
2021-01-31 19:17:41 +0000

    Headings Section

--- XamarinUI.Mobile/XamarinUI.Mobile/ViewModels/HomeViewModel.cs
+++ XamarinUI.Mobile/XamarinUI.Mobile/ViewModels/HomeViewModel.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using XamarinUI.Mobile.Models;
 
 namespace XamarinUI.Mobile.ViewModels
 {
@@ -19,16 +20,27 @@ namespace XamarinUI.Mobile.ViewModels
         {
             navigationService = _navigationService;
             userService = _userService;
+            discoverHeadings = new List<DiscoverHeading>();
             Commands.Add("ChangeName", new Command(() => ChangeName()));
         }
 
         public override Task OnAppearingAsync()
         {
+            DiscoverHeadings.Clear();
+            DiscoverHeadings.Add(new DiscoverHeading() { Text = "DAILY MIX", ImageURL = "https://www.musicweek.com/cimages/6f29a0176e1bc5ca28aa893dd1c1987e.jpg", StartColor = Color.FromHex("000000"), EndColor = Color.FromHex("104756"), Scale = 1.1 }) ;
+            DiscoverHeadings.Add(new DiscoverHeading() { Text = "UP & COMING", ImageURL = "https://www.newreleasetoday.com/thum_creater/phpThumb.php?src=../images/artist_images/img_2360.jpg&w=300&h=360&zc=1", StartColor = Color.FromHex("000000"), EndColor = Color.FromHex("d4eff7"), Scale = 1.1 }) ;
             return base.OnAppearingAsync();
         }
 
         #region vmProperties
-        
+        private List<DiscoverHeading> discoverHeadings;
+
+        public List<DiscoverHeading> DiscoverHeadings
+        {
+            get { return discoverHeadings; }
+            set { SetProperty(ref discoverHeadings, value); }
+        }
+
 
         #endregion
 
--- XamarinUI.Mobile/XamarinUI.Mobile/Views/HomeView.xaml.cs
+++ XamarinUI.Mobile/XamarinUI.Mobile/Views/HomeView.xaml.cs
@@ -13,9 +13,67 @@ namespace XamarinUI.Mobile.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomeView : BaseContentPage
     {
+        private double headingOpacity = 0.6;
         public HomeView()
         {
             InitializeComponent();
+            showHeading(1);
         }
+
+        private void Headings1_Tapped(object sender, EventArgs e)
+        {
+            showHeading(1);
+        }
+        private void Headings2_Tapped(object sender, EventArgs e)
+        {
+            showHeading(2);
+        }
+        private void Headings3_Tapped(object sender, EventArgs e)
+        {
+            showHeading(3);
+        }
+        private void Headings4_Tapped(object sender, EventArgs e)
+        {
+            showHeading(4);
+        }
+
+        #region reuseable
+        private void showHeading(int headingID)
+        {
+            hideAllHeadings();
+            if(headingID == 1)
+            {
+                HeadingsUnderline1.IsVisible = true;
+                Headings1.Opacity = 1;
+            }
+            else if (headingID == 2)
+            {
+                HeadingsUnderline2.IsVisible = true;
+                Headings2.Opacity = 1;
+            }
+            else if (headingID == 3)
+            {
+                HeadingsUnderline3.IsVisible = true;
+                Headings3.Opacity = 1;
+            }
+            else if (headingID == 4)
+            {
+                HeadingsUnderline4.IsVisible = true;
+                Headings4.Opacity = 1;
+            }
+        }
+
+        private void hideAllHeadings()
+        {
+            HeadingsUnderline1.IsVisible = false;
+            Headings1.Opacity = headingOpacity;
+            HeadingsUnderline2.IsVisible = false;
+            Headings2.Opacity = headingOpacity;
+            HeadingsUnderline3.IsVisible = false;
+            Headings3.Opacity = headingOpacity;
+            HeadingsUnderline4.IsVisible = false;
+            Headings4.Opacity = headingOpacity;
+        }
+        #endregion
     }
 }
