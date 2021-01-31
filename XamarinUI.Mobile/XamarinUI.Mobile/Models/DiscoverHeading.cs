682e910201dde7536a29b21520ae83209c7e176f
Ryan Gaudion <rgaudion@gmail.com>
2021-01-31 19:17:41 +0000

    Headings Section

---
+++ XamarinUI.Mobile/XamarinUI.Mobile/Models/DiscoverHeading.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XamarinUI.Mobile.Models
+{
+    public class DiscoverHeading
+    {
+        public string Text { get; set; }
+        public string ImageURL { get; set; }
+        public Color StartColor { get; set; }
+        public Color EndColor { get; set; }
+        public double Scale { get; set; }
+
+    }
+}
